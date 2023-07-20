ab6696c07b98b54fb5d167f4352ea4811c1d3fc3
lind <bfyxzls@sina.com>
2023-07-20 21:07:59 +0800

    更新代码漏洞和不规范写法

--- lind-framework-start/lind-common-core/src/main/java/com/lind/common/core/util/BeanValidatorUtils.java
+++ lind-framework-start/lind-common-core/src/main/java/com/lind/common/core/util/BeanValidatorUtils.java
@@ -1,8 +1,6 @@
 package com.lind.common.core.util;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Validator;
+import javax.validation.*;
 import java.util.Set;
 
 /**
@@ -11,13 +9,14 @@ import java.util.Set;
  * @author ruoyi
  */
 public class BeanValidatorUtils {
+    private static ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
 
-	public static void validateWithException(Validator validator, Object object, Class<?>... groups)
-			throws ConstraintViolationException {
-		Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
-		if (!constraintViolations.isEmpty()) {
-			throw new ConstraintViolationException(constraintViolations);
-		}
-	}
+    public static void validateWithException(Object object, Class<?>... groups)
+            throws ConstraintViolationException {
+        Set<ConstraintViolation<Object>> constraintViolations = validatorFactory.getValidator().validate(object, groups);
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
 
 }
--- lind-framework-start/lind-common-core/src/test/java/com/lind/common/core/constraintvalidator/ConstraintValidatorTest.java
+++ lind-framework-start/lind-common-core/src/test/java/com/lind/common/core/constraintvalidator/ConstraintValidatorTest.java
@@ -3,10 +3,6 @@ package com.lind.common.core.constraintvalidator;
 import com.lind.common.core.util.BeanValidatorUtils;
 import org.junit.Test;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
 /**
  * @author lind
  * @date 2023/7/20 16:32
@@ -15,15 +11,13 @@ import javax.validation.ValidatorFactory;
 
 public class ConstraintValidatorTest {
 
-	@Test
-	public void validUserEmail() {
-		User user = new User();
-		user.setEmail("123@sina.com");
-		// 创建一个验证器工厂
-		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-		Validator validator = validatorFactory.getValidator();
-		BeanValidatorUtils.validateWithException(validator, user);
-		System.out.println(user.getEmail());
-	}
+    @Test
+    public void validUserEmail() {
+        User user = new User();
+        user.setEmail("123@sina.com");
+        // 创建一个验证器工厂
+        BeanValidatorUtils.validateWithException(user);
+        System.out.println(user.getEmail());
+    }
 
 }
