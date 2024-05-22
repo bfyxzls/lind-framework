563cc052d0bbfe334475afd0ccab09f59ce3c618
zhangzhanling <bfyxzls@sina.com>
2024-05-22 11:16:55 +0800

    更新es项目为springboot2.6

--- lind-framework-start/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/ElasticsearchRestTemplateTest.java
+++ lind-framework-start/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/ElasticsearchRestTemplateTest.java
@@ -4,17 +4,23 @@ import com.lind.elasticsearch.entity.EsDto;
 import com.lind.elasticsearch.repository.EsRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.AggregationsContainer;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.Query;
 
-import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
-
+/**
+ * ElasticsearchRestTemplate提供建立索引和复杂的查询功能.
+ */
 @SpringBootTest
 @Slf4j
 public class ElasticsearchRestTemplateTest {
@@ -32,26 +38,50 @@ public class ElasticsearchRestTemplateTest {
 		indexOperations.putMapping(indexOperations.createMapping(EsDto.class));
 	}
 
+	// matchQuery
 	@Test
 	public void simpleQuery() {
-		Query query = new NativeSearchQueryBuilder().withQuery(matchAllQuery()).build();
-
+		Query query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchQuery("desc", "中国")).build();
 		SearchHits<EsDto> searchHits = elasticsearchRestTemplate.search(query, EsDto.class);
-
 		searchHits.getSearchHits().forEach(searchHit -> {
-			System.out.println(searchHit.getContent().toString());
+			System.out.println(searchHit.getContent());
 		});
 	}
 
+	// termQuery
 	@Test
 	public void searchByName() {
 		Query query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.termQuery("application", "lind")).build();
-
 		SearchHits<EsDto> searchHits = elasticsearchRestTemplate.search(query, EsDto.class);
-
 		searchHits.getSearchHits().forEach(searchHit -> {
-			System.out.println(searchHit.getContent().toString());
+			System.out.println(searchHit.getContent());
 		});
+	}
+
+	// 分组聚合
+	@Test
+	public void groupBySex() {
+		// 构建分组聚合查询条件
+		Query searchQuery = new NativeSearchQueryBuilder()
+				.addAggregation(AggregationBuilders.terms("group_by_sex").field("sex")).build();
+
+		// 执行查询
+		SearchHits<EsDto> searchHits = elasticsearchRestTemplate.search(searchQuery, EsDto.class);
+		AggregationsContainer<?> aggregationsContainer = searchHits.getAggregations();
+		if (aggregationsContainer != null) {
+			Aggregations aggregations = (Aggregations) aggregationsContainer.aggregations();
+			for (Aggregation aggregation : aggregations) {
+				if (aggregation instanceof Terms && "group_by_sex".equals(aggregation.getName())) {
+					for (Terms.Bucket entry : ((Terms) aggregation).getBuckets()) {
+						String key = entry.getKeyAsString();
+						long docCount = entry.getDocCount();
+						System.out.println("Key: " + key + ", Doc count: " + docCount);
+					}
+				}
+			}
+
+		}
+
 	}
 
 }
--- lind-framework-start/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsRepositoryTest.java
+++ lind-framework-start/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
+ * springboot2.6.0+spring-boot-starter-data-elasticsearch2.6.0+spring-data-elasticsearch4.3.0
+ * 增，删，改和主键查询
  * @author lind
  * @date 2024/5/21 17:17
  * @since 1.0.0
@@ -17,10 +19,12 @@ public class EsRepositoryTest {
 	@Autowired
 	EsRepo esRepo;
 
+	// 如果索引不存在，通过save()方法是可以根据实体上的注解建立索引的
 	@Test
 	public void test_insert() {
 		EsDto esDto = new EsDto();
-		esDto.setId("1");
+		esDto.setId("4");
+		esDto.setSex(1);
 		esDto.setEmail("bfyxzls@sina.com");
 		esRepo.save(esDto);
 	}
@@ -30,6 +34,7 @@ public class EsRepositoryTest {
 		EsDto esDto = new EsDto();
 		esDto.setId("1");
 		esDto.setName("bfyxzls");
+		esDto.setDesc("中华人民共和国，伟大复兴之路");
 		esRepo.save(esDto);
 	}
 
